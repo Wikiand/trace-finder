a87692c4b85274003963d91bff70384d4db07332
Nyambura <nyamburacn7@gmail.com>
2026-09-12 17:46:17 +0300

    Add argument validation exception

---
+++ src/main/java/TraceFinderArgumentException.java
@@ -0,0 +1,10 @@
+public class TraceFinderArgumentException extends Exception {
+
+    public TraceFinderArgumentException(String message) {
+        super(message);
+    }
+
+    public TraceFinderArgumentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
