73dd70ad6800575b6d771e2bced70a189d63a0ca
Nyambura <nyamburacn7@gmail.com>
2026-09-12 17:28:30 +0300

    Add rulebook exception

---
+++ src/main/java/RulebookException.java
@@ -0,0 +1,10 @@
+public class RulebookException extends Exception {
+
+    public RulebookException(String message) {
+        super(message);
+    }
+
+    public RulebookException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
