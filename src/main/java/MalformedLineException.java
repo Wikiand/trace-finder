5e141d5c5bcc2b1ae867640c82593c6d65908229
Nyambura <nyamburacn7@gmail.com>
2026-09-12 17:16:53 +0300

    Add malformed line exception

---
+++ src/main/java/MalformedLineException.java
@@ -0,0 +1,19 @@
+public class MalformedLineException extends Exception {
+
+    private final int lineNumber;
+    private final String rawContent;
+
+    public MalformedLineException(int lineNumber, String rawContent) {
+        super("Malformed log line at line " + lineNumber);
+        this.lineNumber = lineNumber;
+        this.rawContent = rawContent;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public String getRawContent() {
+        return rawContent;
+    }
+}
